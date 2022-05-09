d7c9c4071583589797855037ebee4dee72a5eb4f
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-09 19:58:49 -0300

    feat: add TransactionsTable core component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,40 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Comprar Elden Ring</td>
+            <td className="withdraw">-R$250,99</td>
+            <td>Games</td>
+            <td>20/03/2022</td>
+          </tr>
+
+          <tr>
+            <td>Freela website</td>
+            <td className="deposit">R$1500,99</td>
+            <td>Trabalho</td>
+            <td>20/03/2022</td>
+          </tr>
+
+          <tr>
+            <td>Comprar Dark Souls</td>
+            <td className="deposit">R$100,99</td>
+            <td>Games</td>
+            <td>20/03/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
