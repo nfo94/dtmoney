2dff4e3491e08f14a0d035b770274775c77225ab
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-10 21:06:31 -0300

    refactor: manage modal state from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
+
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
-
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <Dashboard />
     </>
   );
