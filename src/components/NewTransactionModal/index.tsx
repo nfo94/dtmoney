55a9305e15616fafeccabc6189ab9707e3a8bcb3
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-12 22:36:45 -0300

    feat: add close button to transaction modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import Modal from "react-modal";
 
+import closeImg from "../../assets/close.svg";
 import { Container } from "./styles";
 
 Modal.setAppElement("#root");
@@ -20,6 +21,13 @@ export function NewTransactionModal({
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
       <Container>
         <h2>Cadastrar transação</h2>
         <input type="text" placeholder="Título" />
