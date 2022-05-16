a37695b6c0e176b771c95d7b101b41b1a3cf5a39
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-16 20:44:07 -0300

    style: refactor button with it's own constant

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,38 +1,38 @@
-import styled from 'styled-components'
-import {darken} from 'polished';
+import styled from "styled-components";
+import { darken } from "polished";
 
 export const Container = styled.form`
-  h2{
+  h2 {
     color: var(--text-title);
     font-size: 1.5rem;
     margin-bottom: 2rem;
   }
 
-  input{
+  input {
     width: 100%;
     padding: 0 1.5rem;
     height: 4rem;
     border-radius: 0.25rem;
-    border: 1px solid #D7D7D7;
-    background: #E7E9EE;
+    border: 1px solid #d7d7d7;
+    background: #e7e9ee;
     font-weight: 400;
-   font-size: 1rem ;
+    font-size: 1rem;
 
-   &::placeholder{
-     color: var(--text-body);
-   }
+    &::placeholder {
+      color: var(--text-body);
+    }
 
-   & + input{
-     margin-top: 1rem;
-   }
+    & + input {
+      margin-top: 1rem;
+    }
   }
 
-  button[type="submit"]{
+  button[type="submit"] {
     width: 100%;
     padding: 0 1.5rem;
     height: 4rem;
     background: var(--green);
-    color: #FFF;
+    color: #fff;
     border-radius: 0.25rem;
     border: 0;
     font-size: 1rem;
@@ -40,8 +40,8 @@ export const Container = styled.form`
     transition: filter 0.2s;
     font-weight: 600;
 
-    &:hover{
-      filter:brightness(0.9);
+    &:hover {
+      filter: brightness(0.9);
     }
   }
 `;
@@ -51,31 +51,35 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid #D7D7D7;
-    border-radius: 0.25rem;
-    background: transparent;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: border-color 0.2s;
+interface RadioBoxProps {
+  isActive: boolean;
+}
 
-    &:hover{
-      border-color: ${darken(0.1,'#D7D7D7')};
-    }
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+  background: transparent;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: border-color 0.2s;
 
-    img{
-      width: 28px;
-      height: 20px;
-    }
-
-    span{
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  &:hover {
+    border-color: ${darken(0.1, "#D7D7D7")};
   }
-`
+
+  img {
+    width: 28px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
+`;
