16c7c4908dd3497239e3f84db161f5c0afe2b838
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-16 20:52:30 -0300

    style: add activeColor prop to style in buttons

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from "polished";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form`
   h2 {
@@ -53,15 +53,24 @@ export const TransactionTypeContainer = styled.div`
   gap: 0.5rem;
 `;
 
+const colors = {
+  green: "#33CC95",
+  red: "#E52E4D",
+};
+
 interface RadioBoxProps {
   isActive: boolean;
+  activeColor: "green" | "red";
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
   border: 1px solid #d7d7d7;
   border-radius: 0.25rem;
-  background: transparent;
+  background: ${(props) =>
+    props.isActive
+      ? transparentize(0.9, colors[props.activeColor])
+      : "transparent"};
   display: flex;
   align-items: center;
   justify-content: center;
