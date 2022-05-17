bc98ed4ff2babf9fca2a148e8ce445cc9e38df4c
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2022-05-16 21:36:15 -0300

    refactor: add fake post route and seed fake api

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Elden Ring",
+          type: "withdraw",
+          category: "Jogo",
+          amount: 299.99,
+          createdAt: new Date(),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "Transaction 1",
-          amount: 400,
-          type: "deposit",
-          category: "Food",
-          createdAt: new Date(),
-        },
-      ];
+      return this.schema.all("transaction");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
     });
   },
 });
